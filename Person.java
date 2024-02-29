872a565a39be42bff12a5d49ab69e01b1c0de2e6
Huỳnh Nguyễn Thành Đạt <117693612+hntd1204@users.noreply.github.com>
2024-02-29 09:09:53 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,57 @@
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    // Constructor
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("John Doe", 30, "123 Main St, Cityville");
+
+        System.out.println("Information about person1:");
+        person1.displayInfo();
+
+        person1.setAge(35);
+        person1.setAddress("456 Elm St, Townsville");
+
+        System.out.println("\nUpdated information about person1:");
+        person1.displayInfo();
+    }
+}
